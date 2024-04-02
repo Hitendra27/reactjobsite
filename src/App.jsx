6f3c8e7a0d6a744c822e744e8cef4af6efb13bcc
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-04-02 21:04:27 +0100

    feat: Implemented ViewAllJobs Component

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,6 @@ const App = () => {
       <Hero />
       <HomeCards />
       <JobListings />
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
     </>
   );
 };
